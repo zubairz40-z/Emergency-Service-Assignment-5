6f487af3604bc81fb07c617a0befa88534c50957
zubairz40-z <zahirzubair740@gmail.com>
2025-10-04 01:45:05 +0600

    trying to do the challenge part

---
+++ index.js
@@ -0,0 +1,83 @@
+let heartCount = 0;
+let coins = 100;
+
+const heartsCountTotal = document.getElementById("heartcount");
+const coinBadge = document.getElementById("coincount");
+
+// Heart button
+document.querySelectorAll('.fa-heart').forEach(icon => {
+  icon.addEventListener('click', function() {
+    heartCount++;
+    heartsCountTotal.innerText = heartCount + " ❤️";
+  });
+});
+
+// Call button
+const historyList = document.getElementById('history-list');
+
+document.querySelectorAll('.call-btn').forEach(button => {
+  button.addEventListener('click', function(e) {
+    e.preventDefault(); // stop page refresh
+
+    const serviceTitle = button.dataset.service;
+    const serviceNumber = button.dataset.number;
+
+    // Check coins
+    if (coins < 20) {
+      alert("Cannot make the call!! Coins not enough !!");
+      return;
+    }
+
+    // Deduct coins
+    coins -= 20;
+    coinBadge.innerText = coins + " 🪙";
+
+  
+    // Show alert
+    alert(`Calling ${serviceTitle}: ${serviceNumber}`);
+  })
+
+})
+
+ let entry = { 
+      service: serviceTitle, 
+      number: serviceNumber, 
+      time: new Date().toLocaleString() 
+    };
+    history.push(entry);
+
+  
+    localStorage.setItem("callHistory", JSON.stringify(history));
+
+  
+    renderHistory();
+
+   
+    alert(`Calling ${serviceTitle}: ${serviceNumber}`);
+ 
+
+// --- clear history button ---
+document.getElementById("cllhistory").addEventListener("click", () => {
+  historyList.innerHTML = "";
+});
+
+let copyCount = 0;
+
+document.querySelectorAll('.copy-btn').forEach(button => {
+  button.addEventListener('click', function() {
+    const card = button.closest('.card'); // find the card
+    const number = card.querySelector('.service-number').innerText; // get the number
+
+    // Copy to clipboard
+    navigator.clipboard.writeText(number).then(() => {
+      copyCount++;
+      alert(`Number ${number} copied!`);
+      
+      // Update button text dynamically
+      button.innerText = `${copyCount} Copy`;
+    }).catch(err => {
+      alert("Failed to copy!");
+      console.error(err);
+    });
+  });
+});
